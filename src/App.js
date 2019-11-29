b04c2da3cb6f3907efa87eee12d78955253d2230
= <=>
2019-11-29 15:37:55 +0400

    Styles are near finished.

--- src/App.js
+++ src/App.js
@@ -5,76 +5,94 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: "Max", age: 28 },
-      { name: "Manu", age: 26 },
-      { name: "Stephanie", age: 22 }
+      { id: 1, name: "Max", age: 28 },
+      { id: 2, name: "Manu", age: 26 },
+      { id: 3, name: "Stephanie", age: 22 }
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: false
   }
-  switchNameHandler = (name) => {
+
+  deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  }
+
+  changeNameHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = { ...this.state.persons[personIndex] };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        { name: name, age: 28 },
-        { name: "Zahid", age: 26 },
-        { name: "Kenan", age: 22 }
-      ]
+      persons: persons
     });
   }
 
-  changeNameHandler = (event) => {
-    console.log(event.target);
-    
+  togglePersonsHandler = () => {
     this.setState({
-      persons: [
-        { name: event.target.value, age: 28 },
-        { name: event.target.value, age: 26 },
-        { name: event.target.value, age: 22 }
-      ]
+      showPersons: !this.state.showPersons
     });
   }
-  
+
   render() {
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px'
-    };
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              click={this.deletePersonHandler.bind(this, index)}
+              changed={(event) => this.changeNameHandler(event, person.id)}
+            />
+          })}
+        </div>
+      );
+    }
+
+    let classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
 
     return (
-      <div className="App">
-        <h1>This is my React Learning application</h1>
-        <p>This is the first paragraph</p>
-        <button 
-        onClick={() => this.switchNameHandler('Orxanilian')}
-        style={style}
-        >
-        Switch Handler
+        <div className="App">
+          <h1>This is my React Learning application</h1>
+          <p className={classes.join(' ')}>This is the first paragraph</p>
+          <button className="btn"
+            onClick={this.togglePersonsHandler}
+          >
+            Toggle Persons
         </button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-          changed={this.changeNameHandler}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Orxan')}
-          changed={this.changeNameHandler}
-        >
-          My hobbies: Racing
-          </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-          changed={this.changeNameHandler}
-        />
-      </div>
+          {persons}
+        </div>
     );
   }
   //yuxaridaki return asagidaki returne cevrilir runtime da.
   //orijinal varianti ise asagidaki kimidir.
   //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'This is React'));
+  //ternary expressiondan istifade ederken bize yalniz if lazimdirsa && isaresinden istifade edirik.
+  //if else zamani ise if evezine ?, else evezine ise : isaresinden isrifade edirik.
+  //class-based componentlerde setState funksiyasi zamani biz her hansi state-i deyiserken digerlerine toxunmasaq
+  //bele diger state-ler itmir. deyisdirdiyimiz state-i digerleri ile merge edir. yeni birlesdirir.
+  //useState funksiyasi zamani ise bir state-i deyiserken diger state-ler itir. bunun ucun spread operatordan
+  //istifade etmek lazimdir (...).
+  //map() funksiyasinda list ucun return yazmaq lazimdi hemise.
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import './Person.css';
+// import './Person.css';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+width: 60%;
+margin: 16px auto;
+border: 1px solid #eee;
+box-shadow: 0 2px 3px #ccc;
+padding: 16px;
+text-align: center;
+@media (min-width: 500px) {
+    width: 450px
+}
+`;
 
 const Person = (props) => {
-    return (
-        <div className="Person">
-            <p onClick={props.click}>Menim adim {props.name} ve menim {props.age} yasim var.</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name}/>
-        </div>
-    )
+return (
+    <StyledDiv>
+        <p onClick={props.click}>Menim adim {props.name} ve menim {props.age} yasim var.</p>
+        <p>{props.children}</p>
+        <input type="text" onChange={props.changed} value={props.name} />
+    </StyledDiv>
+)
 }
 
 export default Person;
